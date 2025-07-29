a12ae3f840de2867b80012cdf2cc70405612fd1b
saengx <157899625+saengx@users.noreply.github.com>
2025-07-30 05:45:01 +0700

    Update set.py

--- miner/set.py
+++ miner/set.py
@@ -10,27 +10,32 @@ def setminer():
 
    banneredit()
    try:
-       print("ชื่อคนงานขุด เช่น \033[93mMiner01\033[00m")
-       name = input("[-n]: ")
+       print("0-5 \033[93mปกติ 3\033[00m")
+       cpu-priority = input("[cpu-priority]: ")
        print("\033[35m-----------------------------------------\033[0m")
 
-       print(f"จำนวนthread \033[93mค่าที่ใส่ได้คือ 0 ถึง {cpu_thread}\033[00m")
-       cpu = int(input("[-t]: "))
+       print(f"ค่า full-acess ip \033[93mเช่น 192.168.1.0/24\033[00m")
+       api-allow = int(input("[api-allow]: "))
+       print("\033[35m-----------------------------------------\033[0m")
+
+       print(f"ค่า bind ip:port \033[93mเช่น 0.0.0.0:4068\033[00m")
+       api-bind = int(input("[api-bind]: "))
        print("\033[35m-----------------------------------------\033[0m")
    except:
             os.system("@cls||clear")
             print("เกิดข้อผิดพลาดโปรดตั้งค่าใหม่!")
             time.sleep(3)
-            os.system("edit-miner")
+            os.system("set")
    push = {
-          'name': name,
-          'cpu': cpu
+          'cpu-priority': cpu-priority,
+          'api-allow': api-allow,
+          'api-bind' : api-bind
           }
-   with open("set-miner/offline.json", "w") as set:
+   with open("setip/set-cpu.json", "w") as set:
      json.dump(push, set, indent=4)
 
 while True:
   banneredit()
-  OffMiner()
+  setminer()
   os.system("run-miner")     
   break
