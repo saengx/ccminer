7d161d89916d5f1854d195556fddc86fda93e63b
saengx <157899625+saengx@users.noreply.github.com>
2025-09-30 04:08:11 +0700

    Update check.py

--- miner/check.py
+++ miner/check.py
@@ -1,28 +1,36 @@
 import os, time, json
-import socket
-from urllib.request import urlopen
-def autocheck():
- try:
-     with open("setip/ip.json", encoding="utf-8") as set:
+import requests
+def download_file(url, save_path):
+    try:
+        print("\033[92mกำลังเชื่อมต่อกับ GITHUB \033[0m")
+        #print("\033[92mกำลังเชื่อมต่อกับ HTTP-SERVER \033[0m")
+        print("\033[1;32;40m")
+        os.system("figlet -f digital connect-to-github")
+        print("\033[00m\n")
+        response = requests.get (url, stream=True)
+        response.raise_for_status()
+
+        
+        with open(save_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+
+        
+        print("\033[93m----------เชื่อมต่อสำเร็จแล้ว-----------\033[0m")
+        os.system ("chmod +x start && mv start ../../bin")
+        #os.system ("chmod +x start.json && mv start.json start && mv start ../../bin")
+        os.system ("start")
+    except requests.exceptions.RequestException as e:
+        print ("\033[95mไม่พบการเชื่อมต่อ ตรวจสอบอีกครั้งใน 10 วินาที\033[0m")
+        time.sleep(10)
+        os.system ("python3 check.py")
+with open("setip/ip.json", encoding="utf-8") as set:
              load = set.read()
              loads = json.loads(load)
              user = loads['user']
-             print("\n\033[96mตรวจสอบการเชื่อมต่อกับ GITHUB\033[0m\n")
-             response = urlopen('http://raw.githubusercontent.com')
-       #socket.create_connection(('www.raw.githubusercontent.com',80))
-             status = "ok"
-             os.system (f"cd && wget -N --timeout 20 --connect-timeout=30 -t 2 --no-check-certificate https://raw.githubusercontent.com/{user}/miner/main/begin-control.json && chmod +x begin-control.json && ./begin-control.json && run-miner")
- except socket.error as msg:
-    status = "Not connected"
-    print ('\n\033[95mไม่พบการเชื่อมต่อ ตรวจสอบอีกครั้งใน 15 วินาที\033[0m\n')
-    time.sleep(15)
-    os.system ("python3 check.py")
-    #os.system ("run-miner")                                                                                                                                                                                          
-while True:
-      autocheck()
-      break
-else:
-      #os.system("@cls||clear")
-      print ('\n\033[95mการเชื่อมต่อถูกขัดจังหวะ ตรวจสอบอีกครั้งใน 15 วินาที\033[0m\n')
-      time.sleep(15)
-      os.system ("auto")
+             USER = f"{user}"
+#file_url = f"http://{IP}:8080/start.json"
+file_url = f"https://raw.githubusercontent.com/{USER}/miner/main/start"
+local_save_path = "start"
+#local_save_path = "start.json"
+download_file(file_url,local_save_path)
