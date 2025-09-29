ab6ad4e3ff26eb326d659b02ebb3ca5fd2456bcb
saengx <157899625+saengx@users.noreply.github.com>
2025-09-30 03:51:42 +0700

    Update connect.py

--- miner/connect.py
+++ miner/connect.py
@@ -1,23 +1,46 @@
 import os, time, json
-import socket
-from urllib.request import urlopen
-def autoconnect():
+import requests
+def autoconnect(url, filename):
  try:
      with open("setip/ip.json", encoding="utf-8") as set:
              load = set.read()
              loads = json.loads(load)
              user = loads['user']
              file = loads['file']
-             print("\n\033[92mตรวจสอบการเชื่อมต่อค่า config \033[0m\n")
-             response = urlopen('http://raw.githubusercontent.com')
-             #socket.create_connection(('www.google.com',80))
-             status = "ok"
-             #os.system(f"cd set-miner && wget -N --timeout 20 --connect-timeout=30 -t 2 --no-check-certificate https://raw.githubusercontent.com/{user}/miner/main/{file}.json")
- except socket.error as msg:
-    status = "Not connected"
-    print ('\n\033[95mไม่พบการเชื่อมต่อ ตรวจสอบอีกครั้งใน 15 วินาที\033[0m\n')
-    time.sleep(15)
-    os.system ("python3 connect.py")                                                                                                                                                                                            
+             USER = f"{user}"
+             FILE = f"{file}"
+             print("\033[1;32;40m")
+             #print("\n\033[92mเชื่อมต่อกับค่า CONFIG \033[0m\n")
+             os.system("figlet -f digital SETTING")
+             print("\033[00m\n")
+             url = f"https://raw.githubusercontent.com/{USER}/miner/main/{FILE}.json"
+             output_filename = "set-miner/online.json"
+             response = requests.get (url, stream=True)
+             response.raise_for_status() 
+     with open(filename, 'wb') as f:
+         for chunk in response.iter_content(chunk_size=8192):
+             f.write(chunk)
+             print(f"\n\033[93m{url}\033[0m\n")
+             print("\033[1;33;40m")
+             os.system("figlet -f ANSI_Shadow OK")
+             print("\033[00m\n")
+
+ except requests.exceptions.RequestException as e:
+             print ("\n\033[95mไม่พบการเชื่อมต่อ ตรวจสอบอีกครั้งใน 10 วินาที\033[0m\n")
+             time.sleep(10)
+             os.system ("python3 connect.py")
+
+with open("setip/ip.json", encoding="utf-8") as set:
+             load = set.read()
+             loads = json.loads(load)
+             user = loads['user']
+             file = loads['file']
+             USER = f"{user}"
+             FILE = f"{file}"
+url_to_download = f"https://raw.githubusercontent.com/{USER}/miner/main/{FILE}.json"
+output_filename = "set-miner/online.json"
+autoconnect(url_to_download, output_filename)
+#os.system ("start")                                                                                                                                                                                            
 while True:
       autoconnect()
       break
