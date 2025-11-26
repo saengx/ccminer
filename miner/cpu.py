97386744a36c70d710be42afdd37964b2494f92f
saengx <157899625+saengx@users.noreply.github.com>
2025-11-27 03:21:45 +0700

    Update cpu.py

--- miner/cpu.py
+++ miner/cpu.py
@@ -7,7 +7,7 @@ os.system("lscpu|grep -i 'On-line CPU(s) list'")
 print("\033[00m")
 
 print("**********************************************\n")	
-print("\033[36m ccminer CPU High Hashrate  : " 4.0 "\033[00m\n")
+print("\033[92m ccminer CPU High Hashrate  : > 4.0 < \033[00m")
 print("Verushash v2.2 based on ccminer\n")
 print("**********************************************\n")	
    
